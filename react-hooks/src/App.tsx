7e35cfa273681534a46c716a9374b6769ccfc76b
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2024-06-07 07:39:53 +0800

    chore: 添加 react-router-dom, tailwindcss

--- react-hooks/src/App.tsx
+++ react-hooks/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-
+import { Button } from 'antd'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <></>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">react hooks 掘金小册学习</h1>
+      <Button type={'default'}>开始</Button>
+    </>
+  )
 }
 
 export default App
